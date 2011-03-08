e1a9d536441229247fda270d0ebdd7b4e7e80fc2
Espen Graarud <espengra@stud.ntnu.no>
2011-03-08 20:57:18 +0100

    Added some layout stuff

--- source_code/schedule.c
+++
@@ -1,4 +0,0 @@
-75:	//ENOTE: Here we add challenge response authentication values to the OGM if they are set, node
-76:	((struct bat_packet *)forw_node_new->pack_buff)->challenge = my_challenge;
-78:	((struct bat_packet *)forw_node_new->pack_buff)->response = my_response;
-79:	((struct bat_packet *)forw_node_new->pack_buff)->auth_token = my_auth_token;
